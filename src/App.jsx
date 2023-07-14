47a09abfccb78503fe8f5b6ccfcf652e112029ca
dev1amin <aminayad56@gmail.com>
2023-07-13 22:47:32 -0300

    React - Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Init from './Pages/Header';
+import Project from './Pages/Projects';
+import Skills from './Pages/Skills';
+import Footer from './Pages/Footer';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+    <Init />
+    <Project />
+    <Skills />
+    <Footer />
+  </div>
+  );
+}
+
+export default App;
