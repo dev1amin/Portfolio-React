77dbd294d18b913d5e23fcfc3e5d104c63b2ccec
Amin Morais <111761627+dev1amin@users.noreply.github.com>
2023-07-14 08:53:33 -0300

    Add files via upload

---
+++ src/Pages/Experience/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+const Experiences = () => {
+  return (
+    <div className="experiences-container">
+      <h2>Experiências</h2>
+      <div className="experience">
+        <h3>Empresa 1</h3>
+        <p>Descrição da sua experiência na Empresa 1</p>
+      </div>
+      <div className="experience">
+        <h3>Empresa 2</h3>
+        <p>Descrição da sua experiência na Empresa 2</p>
+      </div>
+    </div>
+  );
+};
+
+export default Experiences;
