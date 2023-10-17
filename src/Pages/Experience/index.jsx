079657ba300ce0801cdb74df1a9e6693ae8f1416
Amin Morais <111761627+dev1amin@users.noreply.github.com>
2023-10-17 18:56:02 -0300

    Update index.jsx

--- src/Pages/Experience/index.jsx
+++ src/Pages/Experience/index.jsx
@@ -6,12 +6,12 @@ const Experiences = () => {
     <div className="experiences-container">
       <h2>Experiências</h2>
       <div className="experience">
-        <h3>Empresa 1</h3>
-        <p>Descrição da sua experiência na Empresa 1</p>
+        <h3>BeNuvem</h3>
+        <p>Suporte Técnico</p>
       </div>
       <div className="experience">
-        <h3>Empresa 2</h3>
-        <p>Descrição da sua experiência na Empresa 2</p>
+        <h3>Pentagrama - em atuação</h3>
+        <p>Webdesigner</p>
       </div>
     </div>
   );
